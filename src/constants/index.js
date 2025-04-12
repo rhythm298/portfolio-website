927facb0b56445082726007413dd92849b43ed96
rhythm298 <rhythmmehta29@gmail.com>
2025-04-13 04:12:51 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -155,7 +155,7 @@ const technologies = [
 const experiences = [
   {
     title: "Full-Stack Developer",
-    company_name: "Tekisky",
+    company_name: "TechTrove",
     icon: tekisky,
     iconBg: "#383E56",
     date: "Jan 2023 - present",
